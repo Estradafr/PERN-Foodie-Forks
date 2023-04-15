08f8994b9f5ec6abed53609fb3593cf7bd7ab695
Fernando Estrada <Estradafr@yahoo.com>
2023-04-15 02:58:48 -0700

    TODO: Create the app components!

--- server/seeders/20230415091503-recipes.js
+++ server/seeders/20230415091503-recipes.js
@@ -5,14 +5,18 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert('recipes', [
       {
-        name: 'Bacon Cheeseburger',
+        name: 'Scrambled Eggs',
         author: 'Fernando Estrada',
-        description:
-          'A juicy bacon cheeseburger that will knock your socks off!',
-        ingredients:
-          'Bacon, pepper jack cheese, lettuce, tomato, pickles, burger sauce',
+        description: 'A classic recipe for a quick and easy scrambled egg.',
+        ingredients: 'Eggs, butter, salt, pepper, olive oil.',
         directions:
-          "Mix a tablespoon of mayo with about half a tablespoon of mustard and ketchup. Then you'll cook your burger well done, toast your buns until golden. Add your toppings as desired, make sure to put the 'toppings' on bottom so you get the best bite possible everytime! Enjoy!",
+          '1. Preheat your pan for a min, letting it get hot.\n' +
+          '2. Add in your olive oil and cover the bottom lining of the pan.\n' +
+          '3. Lower the heat and place your butter.\n' +
+          "4. Allow the butter to melt, but don't burn it.\n" +
+          '5. When the butter melts, add in the eggs and stir them slow!.\n' +
+          '6. Season with the salt and pepper.\n' +
+          '7. Plate and enjoy!.\n',
       },
     ]);
   },
